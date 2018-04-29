d0a98042df2b8cecd4fbc94efe6ed112b3fe7d43
Chris Martin <chris.martin@metova.com>
2018-04-29 12:54:14 -0500

    Add tags, upvote object

---
+++ Sources/App/Models/Requests/Event/UserTag.swift
@@ -0,0 +1,44 @@
+//
+//  UserTag.swift
+//  App
+//
+//  Created by Chris Martin on 4/29/18.
+//
+
+import Foundation
+
+struct UserTag {
+    
+    var userID: User.ID
+    var username: String?
+    
+    init?(tag: String) {
+        
+        let values = tag.trimmingCharacters(in: CharacterSet(charactersIn: "<>"))
+        let components = values.split(separator: "|")
+        
+        guard let userID = components.first, userID.hasPrefix("@") else { return nil }
+        let username = components.last
+        
+        self.init(
+            userID: String(userID).trimmingCharacters(in: CharacterSet(charactersIn: "@")),
+            username: username.flatMap(String.init)
+        )
+    }
+    
+    init(userID: User.ID, username: String?) {
+        
+        self.userID = userID
+        self.username = username
+    }
+    
+    var tagValue: String {
+        
+        if let username = username {
+            return "<@\(userID)|\(username)>"
+        }
+        else {
+            return "<@\(userID)>"
+        }
+    }
+}
